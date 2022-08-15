030d69b6e6f56cc140d928560a1ccb4c3955d790
Benny Austin <austin.benny@gmail.com>
2022-08-15 12:11:12 +1000

    Sync

---
+++ elt-framework/ControlDB/bin/Debug/Post Deployment Script/Script.PostDeployment.sql
@@ -0,0 +1,20 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+--:r .\Script1.PostDeployment.sql
+--IngestDefinition
+:r .\datasources\RestAPI\IngestDefinition\PurviewRestAPI.sql
+:r .\datasources\RestAPI\IngestDefinition\AzureRestAPI.sql
+
+--L1TransformDefinition
+:r .\datasources\RestAPI\L1TransformDefinition\L1T_PurviewRestAPI.sql
+:r .\datasources\RestAPI\L1TransformDefinition\L1T_AzureRestAPI.sql
